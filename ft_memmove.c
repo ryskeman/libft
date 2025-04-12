127fa43e6b6d6c39e44c77491c2e4c68857ac734
Fernando <fernandof.paris@gmail.com>
2025-04-12 18:20:37 +0200

    Add: ft_memmove.c

---
+++ ft_memmove.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fernafer <fernafer@student.42madrid.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/04/12 17:16:21 by fernafer          #+#    #+#             */
+/*   Updated: 2025/04/12 18:16:04 by fernando         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*d;
+	const unsigned char	*s;
+	size_t		i;
+
+	d = (unsigned char	*)dst;
+	s = (const unsigned char	*)src;
+	i = 0;
+	if (d > s)
+	{
+		while (len > 0)
+		{
+			d[len - 1] = s[len - 1];
+			len--;
+		}
+	}
+	else 
+	{
+		while (i < len)
+		{
+			d[i] = s[i];
+			i++;
+		}
+	}
+	return (dst);
+}
+/*
+int	main() {
+    char	str1[] = "Hola, mundo!";
+    char	str2[] = "Hola, mundo!";
+    char	str3[] = "Hola, mundo!";
+    char	str4[] = "Hola, mundo!";
+
+    // Prueba 1: Solapamiento dst > src
+    ft_memmove(str1 + 2, str1, 10);
+    memmove(str2 + 2, str2, 10);
+    printf("Prueba 1 (dst > src): ft_memmove = %s, memmove = %s\n", str1, str2);
+
+    // Prueba 2: Solapamiento dst < src
+    ft_memmove(str3, str3 + 5, 6);
+    memmove(str4, str4 + 5, 6);
+    printf("Prueba 2 (dst < src): ft_memmove = %s, memmove = %s\n", str3, str4);
+
+    // Prueba 3: Sin solapamiento
+    char	str5[] = "abcdefghij";
+    char	str6[] = "abcdefghij";
+    ft_memmove(str5 + 5, str5, 5);
+    memmove(str6 + 5, str6, 5);
+    printf("Prueba 3 (sin solapamiento): ft_memmove = %s, memmove = %s\n", str5, str6);
+
+    // Prueba 4: len = 0
+    char	str7[] = "abcdefghij";
+    char	str8[] = "abcdefghij";
+    ft_memmove(str7 + 2, str7, 0);
+    memmove(str8 + 2, str8, 0);
+    printf("Prueba 4 (len = 0): ft_memmove = %s, memmove = %s\n", str7, str8);
+
+    // Prueba 5: dst == src
+    char	str9[] = "abcdefghij";
+    char	str10[] = "abcdefghij";
+    ft_memmove(str9, str9, 5);
+    memmove(str10, str10, 5);
+    printf("Prueba 5 (dst == src): ft_memmove = %s, memmove = %s\n", str9, str10);
+
+    return 0;
+}
+*/
--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: fernafer <fernafer@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/09 16:47:24 by fernafer          #+#    #+#             */
-/*   Updated: 2025/04/11 21:13:31 by fernafer         ###   ########.fr       */
+/*   Updated: 2025/04/12 18:09:29 by fernando         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,8 @@
 # include <string.h>
 # include <strings.h>
 # include <stdlib.h>
-# include <bsd/bsd.h>
+//# include <bsd/bsd.h>
+# include <stdio.h>
 # include <stddef.h>
 
 int		ft_isalpha(int c);
@@ -32,5 +33,6 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t size);
 void	*ft_memset(void *s, int c, size_t n);
 void	ft_bzero(void *s, size_t n);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
+void    *ft_memmove(void *dst, const void *src, size_t len);
 
 #endif
